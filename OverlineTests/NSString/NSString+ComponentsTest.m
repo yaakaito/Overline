b015243faf319f71ebb971176ccd7ca30f224f10
KAZUMA Ukyo <yaakaito@gmail.com>
2012-12-16 06:02:18 +0900

    add tests

---
+++ OverlineTests/NSString/NSString+ComponentsTest.m
@@ -0,0 +1,33 @@
+//
+//  Overline - NSString+ComponentsTest.m
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#import "NSString+Components.h"
+
+@interface NSString_ComponentsTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSString_ComponentsTest
+
+- (void)testComponentsByInnerAndOuter
+{
+    NSString *base = @"hoge=fuga&piyo=foo";
+    NSDictionary *actual = [base componentsSeparatedByInnerString:@"=" andOuterString:@"&"];
+    NSDictionary *expect = @{
+            @"hoge" : @"fuga",
+            @"piyo" : @"foo"
+    };
+    assertThat(actual, equalTo(expect));
+}
+
+@end
---
+++ OverlineTests/NSURL/NSURL+ComponentsTest.m
@@ -0,0 +1,32 @@
+//
+//  Overline - NSURL+ComponentsTest.m
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import "NSURL+Components.h"
+
+@interface NSURL_ComponentsTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSURL_ComponentsTest
+
+- (void)testQueryComponents
+{
+    NSURL *url = [NSURL URLWithString:@"http://example.com/?hoge=fuga&piyo=foo"];
+    NSDictionary *actual = [url queryComponents];
+    NSDictionary *expect = @{
+            @"hoge" : @"fuga",
+            @"piyo" : @"foo"
+    };
+    assertThat(actual, equalTo(expect));
+}
+
+@end
