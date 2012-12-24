cc7369c1e3e5aabefc89bfc80a4da96b8d5e6179
KAZUMA Ukyo <yaakaito@gmail.com>
2012-12-25 05:47:09 +0900

    added NSBundle json load shorthand

---
+++ OverlineTests/NSBundle/NSBundle+ShorthandTest.m
@@ -0,0 +1,27 @@
+//
+//  Overline - NSBundle+ShorthandTest.m
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#import "NSBundle+Shorthand.h"
+
+@interface NSBundle_ShorthandTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSBundle_ShorthandTest
+
+- (void)testJSONForResourceName {
+    id JSON = [[NSBundle bundleForClass:[self class]] JSONForResourceName:@"test_json"];
+    assertThat(JSON, equalTo(@{@"hoge":@"fuga"}));
+}
+
+@end
