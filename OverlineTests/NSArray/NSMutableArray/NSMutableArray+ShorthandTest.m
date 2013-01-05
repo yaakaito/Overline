1a4a256fa49c8d8bdf647f35aaad63afb7986adc
KAZUMA Ukyo <yaakaito@gmail.com>
2013-01-05 18:28:24 +0900

    added insertObjects:atIndex:

---
+++ OverlineTests/NSArray/NSMutableArray/NSMutableArray+ShorthandTest.m
@@ -0,0 +1,27 @@
+//
+//  Overline - NSMutableArray+ShorthandTest.m
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import "NSMutableArray+Shorthand.h"
+
+@interface NSMutableArray_ShorthandTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSMutableArray_ShorthandTest
+
+- (void)testInsertObjects {
+    NSMutableArray *marray = [NSMutableArray arrayWithArray:@[@1,@2,@3]];
+    [marray insertObjects:@[@4,@5,@6] atIndex:1];
+    assertThat(marray, equalTo(@[@1,@4,@5,@6,@2,@3]));
+}
+
+@end
