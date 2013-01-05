7b08622b7f3d33f18655fd0781972e1ee722ad9c
KAZUMA Ukyo <yaakaito@gmail.com>
2013-01-05 18:17:44 +0900

    added base64 encoding utilities

---
+++ OverlineTests/NSData/NSData+Base64EncodeTest.m
@@ -0,0 +1,31 @@
+//
+//  Overline - NSData+Base64EncodeTest.m
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import "NSData+Base64Encode.h"
+
+
+@interface NSData_Base64EncodeTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSData_Base64EncodeTest
+
+- (void)testBase64Encode {
+    NSData *data = [@"a" dataUsingEncoding:NSUTF8StringEncoding];
+    assertThat([data base64Encode], equalTo(@"YQ=="));
+}
+
+- (void)testStringByEncoding {
+    NSData *data = [@"a" dataUsingEncoding:NSUTF8StringEncoding];
+    assertThat([data stringUsingEncodingBase64], equalTo(@"YQ=="));
+}
+@end
---
+++ OverlineTests/NSString/NSString+Base64EncodeTest.m
@@ -0,0 +1,52 @@
+//
+//  Overline - NSString+Base64EncodeTest.m
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+//  Created by: yaakaito
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#import "NSString+Base64Encode.h"
+
+@interface NSString_Base64EncodeTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSString_Base64EncodeTest
+
+- (void)testEncodeBase64 {
+    assertThat([@"a" encodeBase64], equalTo(@"YQ=="));
+    assertThat([@"hogehoge" encodeBase64], equalTo(@"aG9nZWhvZ2U="));
+    assertThat([@"あいうえお" encodeBase64], equalTo(@"44GC44GE44GG44GI44GK"));
+}
+
+- (void)testStringByEncodingBase64 {
+    assertThat([@"a" stringByEncodingBase64], equalTo(@"YQ=="));
+    assertThat([@"hogehoge" stringByEncodingBase64], equalTo(@"aG9nZWhvZ2U="));
+    assertThat([@"あいうえお" stringByEncodingBase64], equalTo(@"44GC44GE44GG44GI44GK"));
+}
+
+- (void)testDecodeBase64 {
+    assertThat([@"YQ==" decodeBase64], equalTo(@"a"));
+    assertThat([@"aG9nZWhvZ2U=" decodeBase64], equalTo(@"hogehoge"));
+    assertThat([@"44GC44GE44GG44GI44GK" decodeBase64], equalTo(@"あいうえお"));
+}
+
+- (void)testStringByDecodingBase64 {
+    assertThat([@"YQ==" stringByDecodingBase64], equalTo(@"a"));
+    assertThat([@"aG9nZWhvZ2U=" stringByDecodingBase64], equalTo(@"hogehoge"));
+    assertThat([@"44GC44GE44GG44GI44GK" stringByDecodingBase64], equalTo(@"あいうえお"));
+}
+
+- (void)testDataByDecodingBase64 {
+    NSData *data = [@"44GC44GE44GG44GI44GK" dataUsingDecodingBase64];
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    assertThat(str, equalTo(@"あいうえお"));
+}
+
+@end
