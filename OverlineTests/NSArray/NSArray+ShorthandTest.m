fe2a98c3e997bd418033ac9548003597a968f98d
KAZUMA Ukyo <yaakaito@gmail.com>
2013-02-08 23:43:10 +0900

    add NSArray::isEmpty

---
+++ OverlineTests/NSArray/NSArray+ShorthandTest.m
@@ -0,0 +1,29 @@
+//
+//  Overline - NSArray+ShorthandTest.m
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+//  Created by: kazuma.ukyo
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import "NSArray+Shorthand.h"
+
+@interface NSArray_ShorthandTest : SenTestCase
+{
+    
+}
+@end
+
+@implementation NSArray_ShorthandTest
+
+- (void)testIsEmpty
+{
+    assertThatBool([(@[]) isEmpty], equalToBool(YES));
+    assertThatBool([[NSArray array] isEmpty], equalToBool(YES));
+    assertThatBool([(@[@1, @2]) isEmpty], equalToBool(NO));
+
+}
+
+@end
